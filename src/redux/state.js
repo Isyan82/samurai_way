10e347ed36993ea6c331e9e215c3a472e1a39761
Isyan <isyan@mail.ru>
2024-01-14 14:42:29 +0300

    unit 37

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,53 +1,56 @@
-let rerenderEntireTree = () => {
-    return null
-}
-
-let state = {
-    profilePage: {
-        posts: [
-            { id: 1, message: 'Hi, how are you?', likesCount: 12 },
-            { id: 2, message: 'It\'s my first post', likesCount: 11 },
-            { id: 3, message: 'Blabla', likesCount: 11 },
-            { id: 4, message: 'Dafa', likesCount: 11 }
-        ],
-        newPostText: 'It-Kamasutra!'
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                { id: 1, message: 'Hi, how are you?', likesCount: 12 },
+                { id: 2, message: 'It\'s my first post', likesCount: 11 },
+                { id: 3, message: 'Blabla', likesCount: 11 },
+                { id: 4, message: 'Dafa', likesCount: 11 }
+            ],
+            newPostText: 'It-Kamasutra!'
+        },
+        dialogsPage: {
+            dialogs: [
+                { id: 1, name: 'Dimych' },
+                { id: 2, name: 'Andrew' },
+                { id: 3, name: 'Sveta' },
+                { id: 4, name: 'Sasha' },
+                { id: 5, name: 'Viktor' },
+                { id: 6, name: 'Valera' }
+            ],
+            messages: [
+                { id: 1, message: 'Hi' },
+                { id: 2, message: 'How is your it-kamasutra?' },
+                { id: 3, message: 'Yo' },
+                { id: 4, message: 'Yo' },
+                { id: 5, message: 'Yo' }
+            ]
+        },
+        sidebar: {}
     },
-    dialogsPage: {
-        dialogs: [
-            { id: 1, name: 'Dimych' },
-            { id: 2, name: 'Andrew' },
-            { id: 3, name: 'Sveta' },
-            { id: 4, name: 'Sasha' },
-            { id: 5, name: 'Viktor' },
-            { id: 6, name: 'Valera' }
-        ],
-        messages: [
-            { id: 1, message: 'Hi' },
-            { id: 2, message: 'How is your it-kamasutra?' },
-            { id: 3, message: 'Yo' },
-            { id: 4, message: 'Yo' },
-            { id: 5, message: 'Yo' }
-        ]
+    _callSubscriber() {
+        return null
     },
-    sidebar: {}
+    getState() {
+        return this._state
+    },
+    addPost() {
+        let newPost = {
+            id: this._state.profilePage.posts.length + 1,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer
+    }
 }
 
-export const addPost = () => {
-    let newPost = {
-        id: state.profilePage.posts.length + 1,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer
-}
-
-export default state;
+export default store;
